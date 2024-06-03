f5832cf1d80542ca9c767a17298f8f9f4d3d9b25
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-03 01:52:52 -0500

    register route complete

--- server/server/chance/views.py
+++ server/server/chance/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from . forms import CreateUserForm
 
 def homepage(request):
     return render(request, 'chance/index.html')
 
 def register(request):
-    return render(request, 'chance/register.html') 
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('my-login')
+        
+    context = {'registerform': form}
+
+    return render(request, 'chance/register.html', context=context) 
 
 def my_login(request):
     return render(request, 'chance/my-login.html') 
